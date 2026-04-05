524a0df075b58b9a0e07fe4f79f76b6800291769
Larry Laconi <soverylarry@gmail.com>
2026-04-05 19:11:44 -0400

    Update app.py
    Flask cache issue fix

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 # --- CONFIGURATION ---
 PICKS_FILE = 'picks.json'
 HISTORY_FILE = 'history.json'
